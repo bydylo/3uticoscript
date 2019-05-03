8ae3d89f02c412b487690c63cc38178f2c92c615
bydylo <arquivos.dylo@gmail.com>
2019-05-03 18:48:35 -0300

    Update sshplus.sh

--- sshplus.sh
+++ sshplus.sh
@@ -65,7 +65,7 @@ if [ -z "$name" ]; then
   sleep 2
   clear; exit 1
 fi
-IP=$(ip addr | grep 'inet' | grep -v inet6 | grep -vE '127\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | grep -o -E '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | head -1)
+IP=$(wget -qO- ifconfig.co/ip)
 if [[ "$IP" = "" ]]; then
   IP=$(wget -qO- ifconfig.co/ip)
 fi
